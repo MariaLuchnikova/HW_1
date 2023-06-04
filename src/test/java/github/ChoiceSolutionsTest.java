71be7e3ec275e6fa8345078a62e3ef187ab8312e
MariaLuchnikova <luchnikova80@gmail.com>
2023-06-05 00:06:22 +0800

    added solutions

---
+++ src/test/java/github/ChoiceSolutionsTest.java
@@ -0,0 +1,25 @@
+package github;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class ChoiceSolutionsTest {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+    }
+
+    @Test
+    void actionsDragAndDrop() {
+        open("https://github.com/");
+        $("[aria-label='Global']").$(byText("Solutions")).hover();
+        $("[href='/enterprise']").click();
+        $(".enterprise-hero").shouldHave(text("Build like the best"));
+    }
+}
